6e0faf28f5319fa73adb5b4edd2c0024c11d0113
expoya <michael.schiestl@expoya.com>
2025-08-05 09:57:13 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -2,6 +2,6 @@
 export const state = {
   companyData   : {},   // Formular-Payload
   titles        : [],   // ["Titel 1", …]
-  texts         : [],   // ["<html>", …] 1-zu-1 zu titles
+  texts         : [],   // ["<html>", …] 1-zu-1 zu titles.
   runningJobId  : null, // aktuelle Titel-Generierung
 };
