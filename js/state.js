8b89865225f60120df57a54f1bf7d1b41331d85e
expoya <michael.schiestl@expoya.com>
2025-08-05 09:36:22 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,7 @@
+// zentraler, reaktiver Zustand
+export const state = {
+  companyData   : {},   // Formular-Payload
+  titles        : [],   // ["Titel 1", …]
+  texts         : [],   // ["<html>", …] 1-zu-1 zu titles
+  runningJobId  : null, // aktuelle Titel-Generierung
+};
