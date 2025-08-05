7ed6083e4d57646264cd744ebdb6348f9d104de2
expoya <michael.schiestl@expoya.com>
2025-08-05 09:54:14 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@ import { initForm }      from './ui-form.js';
 import { renderExpoList} from './ui-expos.js';
 
 initForm();              // startet alles
-renderExpoList();        // initiale Placeholder-Anzeige
+renderExpoList();        // initiale Placeholder-Anzeige 
