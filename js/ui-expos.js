2eaf5b5664966350a9a8c7aacff81e1d0581e459
expoya <michael.schiestl@expoya.com>
2025-08-12 06:16:40 +0200

    Update ui-expos.js

--- js/ui-expos.js
+++ js/ui-expos.js
@@ -12,7 +12,7 @@ const ladeFloskelnTexte = [
   "Der Mikro-Texter hat gerade aus Versehen einen Reim geschrieben und ist jetzt sehr stolz.",
   "Der SEO-Veredler trägt den Text wie ein rohes Ei ins Korrekturbüro.",
   "Der Mikro-Texter jongliert drei Metaphern – eine fällt runter.“,
-  "Der SEO-Stratege zeigt auf die Wand und sagt: ‚Da muss das Keyword hin.‘",
+  "Der SEO-Stratege zeigt auf die Wand und sagt: Da muss das Keyword hin."
 ];
 
 export function renderExpoList () {
